9dc850f05cb4333f216935d30d1122d515bcd8c0
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-25 12:31:23 -0400

    use render_template

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("first_page.html")
+
+
+@app.route("/fancy")
+def hello_world_fancy():
+    return render_template("second_page.html")
