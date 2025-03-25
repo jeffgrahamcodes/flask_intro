83c6f40ab830e7f02668ec292042a752ed21ee89
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-25 16:14:00 -0400

    data structures &jinja

--- app.py
+++ app.py
@@ -3,36 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return render_template("jinja.html", name="Jeff", template_name="Jinja2")
+class GalileanMoons:
+    def __init__(self, first, second, third, fourth):
+        self.first = first
+        self.second = second
+        self.third = third
+        self.fourth = fourth
 
 
-@app.route("/expressions/")
-def expressions():
-    # interpolation
-    color = "blue"
-    animal_one = "wolf"
-    animal_two = "eagle"
+@app.route("/data_structures/")
+def render_data_structures():
+    movies = ["The Wiz", "Cooley High", "Training Day"]
 
-    # add/subtract
-    orange_amount = 111
-    apple_amount = 11
-    donate_amount = 5
-
-    # string concat
-    first_name = "Jeff"
-    last_name = "Graham"
-
-    kwargs = {
-        "color": color,
-        "animal_one": animal_one,
-        "animal_two": animal_two,
-        "orange_amount": orange_amount,
-        "apple_amount": apple_amount,
-        "donate_amount": donate_amount,
-        "first_name": first_name,
-        "last_name": last_name,
+    car = {
+        "brand": "Cadillac",
+        "model": "El Dorado",
+        "year": 1973,
     }
 
-    return render_template("expressions.html", **kwargs)
+    moons = GalileanMoons("Io", "Europa", "Ganymede", "Callisto")
+
+    kwargs = {"movies": movies, "car": car, "moons": moons}
+
+    return render_template("data_structures.html", **kwargs)
