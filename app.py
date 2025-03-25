398f0ca72e1d92b7020afaca615213018ed30b06
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-25 17:03:31 -0400

    loops, conditionals &jinja

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/conditionals/")
-def conditionals():
-    company = ""
+@app.route("/for-loop/conditionals/")
+def render_conditional_for_loops():
+    user_os = {
+        "Jeff": "MacOS",
+        "Mekhi": "Windows",
+        "Jeffery": "Linux",
+        "Aniyah": "Windows",
+    }
 
-    return render_template("conditionals_basics.html", company=company)
+    return render_template("loops_and_conditionals.html", user_os=user_os)
