cd36848c177f5366387e5cceb3f78d1ab4306fac
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-25 15:33:59 -0400

    add jinja2 template

--- app.py
+++ app.py
@@ -5,9 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("first_page.html")
-
-
-@app.route("/fancy")
-def hello_world_fancy():
-    return render_template("second_page.html")
+    return render_template("jinja.html", name="Jeff", template_name="Jinja2")
